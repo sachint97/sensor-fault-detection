168ce9b24d0dba7cdac30f1b92912c15786bc23c
sachint97 <cssachinshoba@gmail.com>
2023-09-06 12:58:30 +0530

    requirements and setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Sensor-Fault-Detection",
+    version="0.0.1",
+    author="Sachin.T",
+    author_email="cssachinshoba@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
