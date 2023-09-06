83c56c81454bf5b5dabc77514397a35ef502e6c7
sachint97 <cssachinshoba@gmail.com>
2023-09-06 16:14:05 +0530

    folder structure

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements()->List[str]:
+    """ This function returns list of requirements"""
+    requirements_list:List[str] = []
+    with open("requirements.txt","r") as requirements:
+        for requirement in requirements.readlines():
+            requirements_list.append(requirement.split("\n")[0])
+        requirements_list.remove('-e .')
+        print(requirements_list)
+    return requirements_list
+
 
 setup(
-    name="Sensor-Fault-Detection",
+    name="sensor-fault-detection",
     version="0.0.1",
-    author="Sachin.T",
+    author="sachin",
     author_email="cssachinshoba@gmail.com",
     packages=find_packages(),
-    install_requires=[]
+    install_requires=get_requirements()
 )
 
